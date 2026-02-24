05af0676cdcf5f48ff44be10085fd46109bf3ec1
Thomas Smith <thom@thomthesmith.com>
2026-02-23 19:56:57 -0500

    Add label prop for Button

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,10 +2,21 @@ export * from './helpers.js';
 export * from './input.js';
 export * from './select.js';
 
-import { type FC } from 'react';
+import type { FC, ReactNode } from 'react';
 import type { ExtendBuiltin } from './helpers.js';
 
-export const Button: FC<ExtendBuiltin<'button'>> = ({
+export const Button: FC<ExtendBuiltin<'button', { label?: ReactNode }>> = ({
   type = 'button',
+  label,
+  children,
   ...rest
-}) => <button type={type} {...rest} />;
+}) => {
+  if (label !== undefined && label !== null && children !== null ) {
+    throw new TypeError(`can't provide both label prop and children`);
+  }
+  return <button
+    type={type}
+    children={label ?? children}
+    {...rest}
+  />;
+}
